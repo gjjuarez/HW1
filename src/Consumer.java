abde0465f0b30ea13ecb640a9a2a22a880274ce2
george <gjjuarez@miners.utep.edu>
2019-09-02 17:44:56 -0600

    Connection between files established

--- src/Consumer.java
+++ src/Consumer.java
@@ -1,6 +1,42 @@
-public class Consumer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-    public static void main(String[] args) {
-        // write your code here
+public class Consumer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public static void main(String[] args) throws IOException {
+        Consumer server=new Consumer();
+        server.start(6666);
     }
+
+    public void start(int port) throws IOException {
+        System.out.println("Listening on port 6666");
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+        if ("hello consumer".equals(greeting)) {
+            //System.out.println("Listening on port 6666");
+            out.println("hello producer");
+        }
+        else {
+            out.println("unrecognised greeting");
+        }
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
 }
--- src/Producer.java
+++ src/Producer.java
@@ -1,6 +1,38 @@
-public class Producer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Producer {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        System.out.println("Started connection on port 6666");
     }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Producer client = new Producer();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello consumer");
+        System.out.println(response);
+    }
+
 }
