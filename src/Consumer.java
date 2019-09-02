060f4cf82b9f86959701220aff2888a5d49df656
George <gjjuarez@miners.utep.edu>
2019-09-01 22:39:45 -0600

    Initial commit

---
+++ src/Consumer.java
@@ -0,0 +1,6 @@
+public class Consumer {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ src/Producer.java
@@ -0,0 +1,6 @@
+public class Producer {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
