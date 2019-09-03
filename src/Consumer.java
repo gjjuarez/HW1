e6209da74ffadc502c6122c95769f1da719deaa1
george <gjjuarez@miners.utep.edu>
2019-09-02 23:09:02 -0600

    Consumer now handles multiple Producers

--- src/Consumer.java
+++ src/Consumer.java
@@ -6,43 +6,55 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Consumer {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
-
+    
     public static void main(String[] args) throws IOException {
         Consumer server=new Consumer();
         server.start(6666);
-        server.stop();
     }
+
+    private ServerSocket serverSocket;
 
     public void start(int port) throws IOException {
         System.out.println("Listening on port 6666");
         serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        String inputLine;
-        while ((inputLine = in.readLine()) != null){
-            if (inputLine.equals("exit")){
-                out.println("good bye");
-                System.out.println("Good bye");
-                stop();
-                break;
-            }
-            System.out.println(inputLine);
-            out.println(inputLine);
-        }
-
+        while (true)
+            new clientHandler(serverSocket.accept()).start();
     }
 
     public void stop() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
         serverSocket.close();
     }
 
+    private static class clientHandler extends Thread {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public clientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    if (inputLine.equals("exit")) {
+                        System.out.println("Good Bye");
+                        //out.println("bye");
+                        break;
+                    }
+                    System.out.println(inputLine);
+                    //out.println(inputLine);
+                }
+                in.close();
+                out.close();
+                clientSocket.close();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
